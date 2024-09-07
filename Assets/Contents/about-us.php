8df4f6b0b46cc06bee44ed1dab7514836076a5f1
Vansh Thakur <83175304+ProtectorGamer@users.noreply.github.com>
2024-09-07 15:52:52 +0530

    Update about-us.php
    Updated About US

--- Assets/Contents/about-us.php
+++ Assets/Contents/about-us.php
@@ -3,7 +3,17 @@
         <h1>About Us</h1>
         <section class="mission">
             <h2>Our Mission</h2>
-            <p>At Agro Products AI, we aim to revolutionize agriculture through innovative AI technology. Our mission is to provide farmers with cutting-edge tools for plant disease detection, crop health monitoring, and yield optimization, ensuring a healthier and more productive farming experience.</p>
+            <p>Our project focuses on leveraging AI to predict and manage crop diseases efficiently. By utilizing machine learning models trained on agricultural datasets, we aim to provide farmers with accurate disease predictions based on real-time data inputs like leaf images, soil conditions, and weather data.</p>
+        </section>
+        <section class="Main Goals">
+            <h2>This System will Help in the following Ways:</h2>
+            <p>Identify diseases early, allowing for timely intervention.</p>
+            <p>Reduce crop loss, ensuring better yield and sustainability.</p>
+            <p>Provide actionable insights on disease management strategies, empowering farmers to make informed decisions.</p>
+        </section>
+        <section class="Mission Goals">
+            <h2>Project Offering</h2>
+            <p>Our solution offers a scalable, cost-effective, and user-friendly platform for both small-scale and large-scale farmers, contributing to smarter agriculture and food security.</p>
         </section>
         <section class="our-team">
             <h2>Our Team</h2>
@@ -21,7 +31,7 @@
                 <div class="team-member">
                     <img src="https://res.cloudinary.com/gamerlinecnr/image/upload/v1723830387/vansh_jyalxy.jpg" alt="Vansh Thakur">
                     <h3>Vansh Thakur</h3>
-                    <p>Data Scientist</p>
+                    <p>Artificial Intelligence (Data Science/Machine Learning)</p>
                 </div>
             </div>
         </section>
